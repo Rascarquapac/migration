62681080291ec5af51c9fe2669728acffa350625
Rascarquapac <thierry.nancy@gmail.com>
2023-02-21 18:00:17 +0100

    Corrections diverses

--- B3-pricelists.sql
+++ B3-pricelists.sql
@@ -1,8 +1,8 @@
 /* Generate Odoo Price Lists from Dolibarr Pricelist exluding prices not in Dolibarr price list */
 CREATE OR REPLACE VIEW b3_pricelists AS 
 (SELECT
-    "pl_msrp_usd" AS "External ID",
-    "MSRP USD" AS "Pricelist Name",
+    "msrp_eur" AS "External Id"
+    "MSRP EUR" AS "Pricelist Name",
     "Product" AS "Pricelist Items/Apply On",
     CONCAT("[",p.label,"] ",p.ref) AS "Pricelist Items/Product",
     "Fixed Price" AS "Pricelist Items/Compute Price",
@@ -12,29 +12,30 @@ CREATE OR REPLACE VIEW b3_pricelists AS
   FROM
     llx_product_price AS pp
     LEFT JOIN llx_product AS p ON p.rowid = pp.fk_product
-    WHERE pp.price_level = 3 -- FULL EXPORT
+    WHERE pp.price_level = 3 AND (p.tobuy = 1 OR p.tosell = 1)-- FULL EXPORT
     -- WHERE pp.price_level = 3 AND p.rowid IN (126,294,201,200,202,377,118,330,119,391)-- LIGHT EXPORT
-
   GROUP BY p.rowid
   HAVING MAX(pp.date_price)
+  ORDER BY "Pricelist Items/Product"
 
 )
 UNION
   SELECT
-    "pl_msrp_eur" AS "External ID",
-    "MSRP EUR" AS "Pricelist Name",
+    "msrp_usd" AS "External Id"
+    "MSRP USD" AS "Pricelist Name",
     "Product" AS "Pricelist Items/Apply On",
     CONCAT("[",p.label,"] ",p.ref) AS "Pricelist Items/Product",
     "Fixed Price" AS "Pricelist Items/Compute Price",
     MAX(pp.price * 1.2) AS "Pricelist Items/Fixed Price", -- when price_level = 1
     -- pp.price AS "Pricelist Items/Fixed Price",    -- when price_level = 4
-    "EUR" AS "Currency",
+    "USD" AS "Currency",
     "Sales Price" AS "Pricelist Items/Based on"
   FROM
     llx_product_price AS pp
     LEFT JOIN llx_product AS p ON p.rowid = pp.fk_product
-  WHERE pp.price_level = 4 -- FULL EXPORT
+  WHERE pp.price_level = 4  AND (p.tobuy = 1 OR p.tosell = 1)-- FULL EXPORT
   -- WHERE pp.price_level = 4 AND p.rowid IN (126,294,201,200,202,377,118,330,119,391)-- LIGHT EXPORT
   GROUP BY p.rowid
-  HAVING MAX(pp.date_price);
+  HAVING MAX(pp.date_price)
+  ORDER BY "Pricelist Items/Product";
   SELECT * FROM b3_pricelists;
--- checklist.md
+++ checklist.md
@@ -1,7 +1,8 @@
 # Checklist for Dolibarr to Odoo migration
 ## Odoo setup
-1. Activate : Setup/Sales/Pricing/Pricelists/`Advanced price rules` (Pricelists)
-2. Activate : Setup/Inventory/Shipping/`Delivery Method` (HS code, …)
+1. Activate : Setting/Sales/Pricing/Pricelists/`Advanced price rules` (Pricelists)
+2. Activate : Setting/Inventory/Shipping/`Delivery Method` (HS code, …)
+3. Activate : Setting/Invoicing/Currencies (Add USD, GBP,…)
 ## Contacts import
 1. Import : contactsTags
 2. Import : contacts
