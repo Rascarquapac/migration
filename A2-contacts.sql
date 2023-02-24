68e54b3fe59a5be442b6e93bde52d026ee678c74
Rascarquapac <thierry.nancy@gmail.com>
2023-02-24 07:50:22 +0100

    Suppressing faulty line

--- A2-contacts.sql
+++ A2-contacts.sql
@@ -13,7 +13,6 @@ SELECT
     WHEN country.code IN ("AT","BG","HR","CY","CZ","DK","EE","FI","FR","DE","GR","HU","IE","IT","LV","LT","LU","MT","NL","PL","PT","RO","SK","SI","ES","SE","EU") THEN "Régime Intra-Communautaire" 
     ELSE IF(ISNULL(country.code),NULL,"Régime Extra-Communautaire") 
   END AS "property_account_position_id", -- To be checked
-  property_account_position_id
   IFNULL(societe.url,"") AS "Website Link",
   IFNULL(societe.email,"") AS "Email",
   societe.client AS "Customer Rank",
